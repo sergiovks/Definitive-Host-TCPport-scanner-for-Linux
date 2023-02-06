4471de40258d80bb961b4d17a427a34b170423a4
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-02-06 13:54:25 +0100

    Update hostports.sh

--- hostports.sh
+++ hostports.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function ctrl_c(){
-echo -e "\n\n [!] Leaving...\n"
-tput cnorm; exit 1
+  echo -e "\n\n [!] Leaving...\n"
+  tput cnorm; exit 1
 }
 
 #Ctrl+C
@@ -13,25 +13,29 @@ tput civis
 #Main Function
 
 host_scan () {
+  network=$1
   for i in $(seq 2 254); do
-    timeout 1 bash -c "ping -c 1 10.10.0.$i" &>/dev/null
-    if [ $? -eq 0 ]; then
-    for port in $(seq 1 65535); do
-      timeout 1 bash -c "echo '' > /dev/tcp/10.10.0.$i/$port" &>/dev/null
+    (
+      timeout 1 bash -c "ping -c 1 ${network}.$i" &>/dev/null
       if [ $? -eq 0 ]; then
-      echo "[+] Host 10.10.0.$i ACTIVE & PORT $port OPEN" &
+        echo "[+] Host ${network}.$i - ACTIVE"
+        for port in $(seq 1 65535); do
+          timeout 1 bash -c "echo '' > /dev/tcp/${network}.$i/$port" &>/dev/null
+          if [ $? -eq 0 ]; then
+            echo "[+] Host ${network}.$i ACTIVE & PORT $port OPEN"
+          fi
+        done
       fi
-  done; wait
-  else
-    echo "[+] Host 10.10.0.$i - ACTIVE" &
-  fi
-
-  done; wait
+    ) &
+  done
+  wait
 }
 
 tput cnorm
 
 main() {
-host_scan
+  echo -n "Enter the network like this (e.g. 10.10.0): "
+  read network
+  host_scan $network
 }
 main
